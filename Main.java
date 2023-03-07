f0a3b942514a7165d649ccada02a8ea6887e8eb9
OscarLizano001 <98754897+Oscarlizano001@users.noreply.github.com>
2023-03-07 18:41:07 +0000

    Everything

--- Main.java
+++ Main.java
@@ -6,9 +6,45 @@ import java.util.ArrayList;
 class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 
-		ArrayList<String> arrayList = new ArrayList<>();
+		ArrayList<String> names = new ArrayList<>();
 		FileReader fr = new FileReader("names.txt");
 		Scanner fileScanner = new Scanner(fr);
-		
+    Scanner scan = new Scanner(System.in);
+
+      while( fileScanner.hasNextLine()) {
+      names.add(fileScanner.nextLine());
+    }
+    int userInput = 0;
+    while (userInput != 5){
+     System.out.println("Press 1 to learn about salary.");
+     System.out.println("Press 2 to learn about the job.");
+     System.out.println("Press 3 to learn about demand.");
+     System.out.println("Press 4 to view current students.");
+     System.out.println("Press 5 to quit."); 
+
+      userInput = scan.nextInt();
+
+      switch (userInput) {
+        case 1: 
+          System.out.println("$98,345 average salary in South Florida!");
+          break;
+        
+        case 2:
+            System.out.println("US News - 100 Best Jobs");
+            break;
+        case 3:
+            System.out.println("Top 10 Forbes In-Demand Jobs!");
+            break;
+        case 4:
+            System.out.println("Current Students:");
+              for(int i = 0; i < names.size(); i++){
+            System.out.println(names.get(i));
+              }
+          break;
+        case 5:
+            break;
+          
+      }
+    }
 	}
 }
